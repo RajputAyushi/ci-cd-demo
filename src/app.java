85fbadc1dd0182a9d616ccdcd406cf55de3f1528
Ayushi <Ayushi101rajput@gmail.com>
2025-05-16 23:14:36 +0530

    Initial commit

---
+++ src/app.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from CI/CD Pipeline!");
+    }
+
+    public static boolean isWorking() {
+        return true;
+    }
+}
+
---
+++ src/apptest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        Assert.assertTrue(App.isWorking());
+    }
+}
+
